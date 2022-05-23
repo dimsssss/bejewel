83b6adbe443c8b77ba8320039bc7b8643981f601
dimsssss <caruen@hanmail.net>
2022-05-23 15:48:48 +0900

    feat: 웹 어플리케이션 초기화

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({error:'error'})
+});
+
+module.exports = app;
