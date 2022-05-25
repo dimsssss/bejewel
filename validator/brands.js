e2b15f3f8bff903e84f6136e28905c73546e677c
dimsssss <caruen@hanmail.net>
2022-05-25 13:16:08 +0900

    refactor: validaion 분리

--- validator/brands.js
+++ validator/brands.js
@@ -1,20 +1,11 @@
-const { body, validationResult } = require('express-validator');
-const {StatusCodes} = require("http-status-codes");
+const { body } = require('express-validator');
 
 const hasValidBrandId = [
     body('id').isAscii()
 ]
 
-const checkValidationResult = (req, res, next) => {
-    const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(StatusCodes.BAD_REQUEST).send({error: errors.array()});
-    }
-    next();
-}
 
 module.exports = {
-    hasValidBrandId,
-    checkValidationResult
+    hasValidBrandId
 }
