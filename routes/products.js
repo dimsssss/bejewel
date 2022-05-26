af80120c8b552b042bcd9ddfd2fab2632ec5468c
dimsssss <caruen@hanmail.net>
2022-05-26 18:13:59 +0900

    feat: 라우터 연결

--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { hasValidBody, hasValidCategoryIds, hasValidProductId, hasValidProductDetail } = require('../validator/products');
 const { checkValidationResult } = require('../validator/validationResultWrapper');
-const { createProductAndProductCategory, getProductsForCategory, getProductDetail, updateProductDetail } = require('../controllers/productsController');
+const { createProductAndProductCategory, getProductsForCategory, getProductDetail, updateProductDetail, deleteProduct} = require('../controllers/productsController');
 
 router.patch('/', hasValidProductDetail, checkValidationResult, updateProductDetail);
 router.post('/', hasValidBody, checkValidationResult, createProductAndProductCategory);
 router.get('/', hasValidCategoryIds, checkValidationResult, getProductsForCategory);
 router.get('/:productId', hasValidProductId, checkValidationResult, getProductDetail);
+router.delete('/:productId', hasValidProductId, checkValidationResult, deleteProduct);
 
 module.exports = router;
